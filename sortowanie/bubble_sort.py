fcbe454bce70546455d9dbc51e4f96d4608485e0
Jakub Rozkosz <rozkoszjakub01@gmail.com>
2021-11-06 20:48:48 +0100

    bubble&merge sort

---
+++ sortowanie/bubble_sort.py
@@ -0,0 +1,6 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(0, n-1):
+        for j in range(0, n-1-i):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
