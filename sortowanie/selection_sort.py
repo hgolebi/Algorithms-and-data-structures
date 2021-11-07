4064be4fa875600bf1123ffdaef9965f6a346e22
Hubert <hugo7744@gmail.com>
2021-11-07 18:31:14 +0100

    poprawione selection sort

--- sortowanie/selection_sort.py
+++ sortowanie/selection_sort.py
@@ -1,16 +1,7 @@
-arr = [2, 31, 2, 1 ,23, 0, 4, 3]
-
 def SelectionSort(list):
-    it = 0
-    while(it < len(list)-1):
+    for it in range(0, len(list)):
         min_index = it
         for index in range(it+1, len(list)):
             if list[index] < list[min_index]:
                 min_index = index
-        temp = list[it]
-        list[it] = list[min_index]
-        list[min_index] = temp
-        it += 1
-
-SelectionSort(arr)
-print(arr)
+        list[it], list[min_index] = list[min_index], list[it]
