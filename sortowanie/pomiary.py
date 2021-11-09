9dbf79e74a814d243c2632a80c1771c2d295f3c0
Jakub Rozkosz <rozkoszjakub01@gmail.com>
2021-11-09 20:05:15 +0100

    rysowanie wykresow

--- sortowanie/pomiary.py
+++ sortowanie/pomiary.py
@@ -2,27 +2,57 @@ from merge_sort import merge_sort
 from bubble_sort import bubble_sort
 from readfile import GetWordsList
 from quicksort import QuickSort
+from selection_sort import SelectionSort
 import timeit
 
-from sortowanie.selection_sort import SelectionSort
+from matplotlib import pyplot as plt
+import numpy as np
+
+def draw_a_plot(x, y, sort_type):
+    plt.plot(x, y)
+    plt.suptitle(sort_type+" - time complexity")
+    plt.ylabel("Measured time (s)")
+    plt.xlabel("Number of sorted words")
+    plt.show()
 
 file_path = 'pan-tadeusz-nowy.txt'
-words_num=1000
-while(words_num<=10000):
-    arr = GetWordsList(file_path, words_num)
+words_number=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+measured_time=[]
+
+#jak coś to trzeba zamknąć jeden wykres, aby odpalil się kolejny
+
+# bubble sort
+for number in words_number:
+    arr = GetWordsList(file_path, number)
+    merge_time = timeit.timeit(lambda: bubble_sort(arr), number=1)
+    measured_time.append(merge_time)
+
+draw_a_plot(words_number, measured_time, "Bubble sort")
+measured_time=[]
+
+# selection sort
+for number in words_number:
+    arr = GetWordsList(file_path, number)
+    merge_time = timeit.timeit(lambda: SelectionSort(arr), number=1)
+    measured_time.append(merge_time)
+
+draw_a_plot(words_number, measured_time, "Selection sort")
+measured_time=[]
+
+# merge sort
+for number in words_number:
+    arr = GetWordsList(file_path, number)
     merge_time = timeit.timeit(lambda: merge_sort(arr), number=1)
-    print("Merge sort execution time for "+str(words_num)+ " words: " + str(merge_time))
+    measured_time.append(merge_time)
 
-    arr = GetWordsList(file_path, words_num)
-    bubble_time = timeit.timeit(lambda: bubble_sort(arr), number=1)
-    print("Bubble sort execution time for "+str(words_num)+ " words: " + str(bubble_time))
+draw_a_plot(words_number, measured_time, "Merge sort")
+measured_time=[]
 
-    arr = GetWordsList(file_path, words_num)
-    quick_time = timeit.timeit(lambda: QuickSort(arr), number=1)
-    print("Quick sort execution time for "+str(words_num)+ " words: " + str(quick_time))
+# quick sort
+for number in words_number:
+    arr = GetWordsList(file_path, number)
+    merge_time = timeit.timeit(lambda: QuickSort(arr), number=1)
+    measured_time.append(merge_time)
 
-    arr = GetWordsList(file_path, words_num)
-    selection_time = timeit.timeit(lambda: SelectionSort(arr), number=1)
-    print("Selection sort execution time for "+str(words_num)+ " words: " + str(selection_time))
-
-    words_num += 1000
+draw_a_plot(words_number, measured_time, "Quick sort")
+measured_time=[]
