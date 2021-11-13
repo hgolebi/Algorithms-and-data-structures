6c215039cf8a87f6750261ecf669abdc0065d0e6
Jakub Rozkosz <rozkoszjakub01@gmail.com>
2021-11-13 22:50:27 +0100

    pomiary i wygenerowanie wszystkich wykresow

--- sortowanie/pomiary.py
+++ sortowanie/pomiary.py
@@ -8,48 +8,64 @@ import timeit
 from matplotlib import pyplot as plt
 
 def draw_a_plot(x, y, sort_type):
-    plt.plot(x, y)
+    plt.plot(x, y, marker='o')
     plt.suptitle(sort_type+" - time complexity")
     plt.ylabel("Measured time (s)")
     plt.xlabel("Number of sorted words")
     plt.savefig(sort_type+'.png')
+    plt.clf()
 
 file_path = 'pan-tadeusz-nowy.txt'
 words_number=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
-measured_time=[]
+time_bubble=[]
+time_selection=[]
+time_merge=[]
+time_quick=[]
 
 # bubble sort
 for number in words_number:
     arr = GetWordsList(file_path, number)
-    merge_time = timeit.timeit(lambda: bubble_sort(arr), number=1)
-    measured_time.append(merge_time)
+    measured_time = timeit.timeit(lambda: bubble_sort(arr), number=10)
+    measured_time/=10
+    time_bubble.append(measured_time)
 
-draw_a_plot(words_number, measured_time, "Bubble sort")
-measured_time=[]
+draw_a_plot(words_number, time_bubble, "Bubble sort")
 
 # selection sort
 for number in words_number:
     arr = GetWordsList(file_path, number)
-    merge_time = timeit.timeit(lambda: SelectionSort(arr), number=1)
-    measured_time.append(merge_time)
+    measured_time = timeit.timeit(lambda: SelectionSort(arr), number=10)
+    measured_time/=10
+    time_selection.append(measured_time)
 
-draw_a_plot(words_number, measured_time, "Selection sort")
-measured_time=[]
+draw_a_plot(words_number, time_selection, "Selection sort")
 
 # merge sort
 for number in words_number:
     arr = GetWordsList(file_path, number)
-    merge_time = timeit.timeit(lambda: merge_sort(arr), number=1)
-    measured_time.append(merge_time)
+    measured_time = timeit.timeit(lambda: merge_sort(arr), number=1000)
+    measured_time/=1000
+    time_merge.append(measured_time)
 
-draw_a_plot(words_number, measured_time, "Merge sort")
-measured_time=[]
+draw_a_plot(words_number, time_merge, "Merge sort")
 
 # quick sort
 for number in words_number:
     arr = GetWordsList(file_path, number)
-    merge_time = timeit.timeit(lambda: QuickSort(arr), number=1)
-    measured_time.append(merge_time)
+    measured_time = timeit.timeit(lambda: QuickSort(arr), number=1000)
+    measured_time/=1000
+    time_quick.append(measured_time)
 
-draw_a_plot(words_number, measured_time, "Quick sort")
-measured_time=[]
+draw_a_plot(words_number, time_quick, "Quick sort")
+
+# wszystkie na jednym wykresie
+plt.plot(words_number, time_bubble, marker='o', label='Bubble sort')
+plt.plot(words_number, time_selection, marker='o', label='Selection sort')
+plt.plot(words_number, time_merge, marker='o', label='Merge sort')
+plt.plot(words_number, time_quick, marker='o', label='Quick sort')
+plt.legend(prop={'size':8})
+plt.suptitle("All sorting types - time complexity")
+plt.ylabel("Measured time (s)")
+plt.xlabel("Number of sorted words")
+plt.savefig('All-in-one.png')
+plt.clf()
