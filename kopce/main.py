3bf5780c6b5dd9c622cb77148a4fe97528e3af0b
Jakub Rozkosz <rozkoszjakub01@gmail.com>
2021-12-09 19:28:40 +0100

    mierzenie czasu, main, testy

---
+++ kopce/main.py
@@ -0,0 +1,44 @@
+from binary_heap import BinaryHeap
+#from trinary_heap import TrinaryHeap
+#from quaternary_heap import QuaternaryHeap
+from random import randint
+from time_measure import get_time_insert, get_time_delete
+from drawing_plots import draw_a_plot
+import sys
+
+sys.setrecursionlimit(3000)
+
+
+list = []
+for i in range(0, 100001):
+    val = randint(0, 1500000)
+    while val in list:
+        val = randint(0, 1500000)
+    list.append(val)
+
+checkpoints = [i for i in range(10000, 100001, 10000)]
+
+heap_2_times = ([], [])       # insert, delete
+heap_3_times = ([], [])
+heap_4_times = ([], [])
+
+for n in checkpoints:
+
+    heap_2 = BinaryHeap()
+    #heap_3 = TrinaryHeap()
+    #heap_4 = QuaternaryHeap()
+
+    new_list = list[:n]
+
+    heap_2_times[0].append(get_time_insert(heap_2, new_list))
+    heap_2_times[1].append(get_time_delete(heap_2, n))
+
+    #heap_3_times[0].append(get_time_insert(heap_2, new_list))
+    #heap_3_times[1].append(get_time_delete(heap_2, n))
+
+    #heap_4_times[0].append(get_time_insert(heap_2, new_list))
+    #heap_4_times[1].append(get_time_delete(heap_2, n))
+    print(n, "iteration")
+
+draw_a_plot(checkpoints, heap_2_times[0], heap_3_times[0], heap_4_times[0], "creating")
+draw_a_plot(checkpoints, heap_2_times[1], heap_3_times[1], heap_4_times[1], "deleting root")
--- kopce/time_measure.py
+++ kopce/time_measure.py
@@ -1,21 +1,22 @@
 import gc
 import time
 
-def getTime(heap, list, operation):
+def get_time_insert(heap, list):
     gc_old = gc.isenabled()
     gc.disable()
-    if operation == 'insert':
-        start = time.process_time()
-        for elem in list:
-            heap.insert_value(elem)
-        stop = time.process_time()
-        return stop - start
-
-    if operation == 'delete':
-        start = time.process_time()
-        for elem in list:
-            heap.delete_root(elem)
-        stop = time.process_time()
-        return stop - start
-
+    start = time.process_time()
+    for elem in list:
+        heap.insert_value(elem)
+    stop = time.process_time()
     gc.enable()
+    return stop - start
+
+def get_time_delete(heap, number):
+    gc_old = gc.isenabled()
+    gc.disable()
+    start = time.process_time()
+    for i in range(number):
+        heap.delete_root()
+    stop = time.process_time()
+    gc.enable()
+    return stop - start
